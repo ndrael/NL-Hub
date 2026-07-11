48f6bcdd478fc77736186ff300e6cf9057e4dac4
ndrael <ndrael.org@gmail.com>
2026-07-12 00:50:18 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,4 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-local Logic = loadstring(game:HttpGet("https://raw.githubusercontent.com/ndrael/NL-Hub/refs/heads/main/logic.lua"))()
 
 WindUI:AddTheme({
     Name = "Default",
@@ -87,7 +86,7 @@ local Button = EspTab:Button({
     Title = "Survivors",
     Locked = false,
     Callback = function()
-        Logic.ESPSurvivors()
+        -- ...
     end
 })
 
@@ -95,7 +94,7 @@ local Button = EspTab:Button({
     Title = "Killer",
     Locked = false,
     Callback = function()
-        Logic.ESPKiller()
+        -- ...
     end
 })
 
@@ -103,7 +102,7 @@ local Button = EspTab:Button({
     Title = "Spectator",
     Locked = false,
     Callback = function()
-        Logic.ESPSpectator()
+        -- ...
     end
 })
 
