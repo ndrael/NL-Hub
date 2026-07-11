a4fb9d8c69530ee274a5063f7d842198bb73c1fd
ndrael <ndrael.org@gmail.com>
2026-07-11 21:25:00 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,61 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+WindUI:AddTheme({
+    Name = "Default",
+    Accent = Color3.fromHex("#1a1611"),
+    Background = Color3.fromHex("#0a0908"),
+    BackgroundTransparency = 0,
+    Outline = Color3.fromHex("#d4af37"),
+    Text = Color3.fromHex("#f0d78c"),
+    Placeholder = Color3.fromHex("#8a7645"),
+    Button = Color3.fromHex("#2b2410"),
+    Icon = Color3.fromHex("#ffd700"),
+    Hover = Color3.fromHex("#ffdf70"),
+    WindowBackground = Color3.fromHex("#0a0908"),
+    WindowShadow = Color3.fromHex("#000000"),
+    DialogBackground = Color3.fromHex("#0a0908"),
+    DialogBackgroundTransparency = 0,
+    DialogTitle = Color3.fromHex("#ffd700"),
+    DialogContent = Color3.fromHex("#f0d78c"),
+    DialogIcon = Color3.fromHex("#ffd700"),
+    WindowTopbarButtonIcon = Color3.fromHex("#ffd700"),
+    WindowTopbarTitle = Color3.fromHex("#ffd700"),
+    WindowTopbarAuthor = Color3.fromHex("#8a7645"),
+    WindowTopbarIcon = Color3.fromHex("#ffd700"),
+    TabBackground = Color3.fromHex("#1a1611"),
+    TabTitle = Color3.fromHex("#f0d78c"),
+    TabIcon = Color3.fromHex("#ffd700"),
+    ElementBackground = Color3.fromHex("#1a1611"),
+    ElementTitle = Color3.fromHex("#f0d78c"),
+    ElementDesc = Color3.fromHex("#8a7645"),
+    ElementIcon = Color3.fromHex("#ffd700"),
+    PopupBackground = Color3.fromHex("#0a0908"),
+    PopupBackgroundTransparency = 0,
+    PopupTitle = Color3.fromHex("#ffd700"),
+    PopupContent = Color3.fromHex("#f0d78c"),
+    PopupIcon = Color3.fromHex("#ffd700"),
+    Toggle = Color3.fromHex("#1a1611"),
+    ToggleBar = Color3.fromHex("#ffd700"),
+    Checkbox = Color3.fromHex("#1a1611"),
+    CheckboxIcon = Color3.fromHex("#f0d78c"),
+    Slider = Color3.fromHex("#1a1611"),
+    SliderThumb = Color3.fromHex("#ffd700"),
+})
+
+local Window = WindUI:CreateWindow({
+    Title = "My Super Hub",
+    Icon = "door-open",
+    Author = "by .ftgs and .ftgs",
+    Theme = "Default"
+})
+
+Window:EditOpenButton({
+    Title = "NL Hub",
+    Icon = "crown",
+    CornerRadius = UDim.new(0, 16),
+    StrokeThickness = 2,
+    Color = ColorSequence.new(Color3.fromHex("#FFD700")),
+    OnlyMobile = false,
+    Enabled = true,
+    Draggable = true,
+})
